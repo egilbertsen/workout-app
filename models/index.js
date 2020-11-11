44191603ffcf108d4fd973da23d9a8942df5902c
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2020-11-10 21:22:02 -0800

    Fixing some route issues

--- models/index.js
+++ models/index.js
@@ -3,21 +3,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  excercises: [
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
   {
     type: {
       type: String,
       trim: true,
-      required: "Enter an excercise type"
+      required: "Please enter an exercise type"
     },
     name: {
       type: String,
       trim: true,
-      required: "Enter a name for excercise"
+      required: "Please enter a name for exercise"
     },
     duration: {
       type: Number,
-      required: "Please enter an excercise duration"
+      required: "Please enter an exercise duration"
     },
     weight: {
       type: Number
@@ -31,15 +35,11 @@ const workoutSchema = new Schema({
     distance: {
       type: Number
     }
-  }],
-  day: {
-    type: Date,
-    default: Date.now
-  }
+  }]
 
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("workouts", workoutSchema);
 
 module.exports = Workout;
 
--- server.js
+++ server.js
@@ -11,9 +11,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useNewUrlParser: true
 });
 
 
@@ -28,15 +27,16 @@ app.get("/", (req,res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/excercise", (req,res) => {
-  res.sendFile(path.join(__dirname, "public", "excercise.html"));
+app.get("/exercise", (req,res) => {
+  res.sendFile(path.join(__dirname, "public", "exercise.html"));
 });
 
 app.get("/stats", (req,res) => {
   res.sendFile(path.join(__dirname, "public", "stats.html"))
 })
-// API ROUTES
 
+
+// API ROUTES
 app.post("/api/workouts", (req, res) => {
   let data = req.body;
   db.Workout.create({
@@ -48,42 +48,43 @@ app.post("/api/workouts", (req, res) => {
   })
 });
 
-
+// https://docs.mongodb.com/manual/reference/operator/update/push/
 app.put("/api/workouts/:id", (req,res) => {
   let url = req.params;
   let data = req.body;
 
-  db.Workout.updateOne({
-    _id: url.id
-  }, {
-    $push: {excercises: [
+  db.Workout.update(
+    {_id: url.id}, 
+    {$push: { exercises: [
       {
-        "type": data.type,
-        "name": data.name,
-        "duration": data.duration,
-        "weight": data.weight,
-        "sets": data.sets,
-        "reps": data.reps,
-        "distance": data.distance
+        type: data.type,
+        name: data.name,
+        duration: data.duration,
+        weight: data.weight,
+        sets: data.sets,
+        reps: data.reps,
+        distance: data.distance
       }
-    ]}
-  }).then(update => {
+    ]}}
+    ).then(update => {
     res.json(update);
   }).catch(err => {
     res.json(err);
   })
 });
 
+
 app.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({}).then(workoutRes => {
+  db.Workout.find().then(workoutRes => {
     res.json(workoutRes);
   }).catch(err => {
     res.json(err);
   })
 })
 
+// https://stackoverflow.com/questions/10920651/get-the-latest-record-from-mongodb-collection/53474483
 app.get("/api/workouts", (req,res) => {
-  db.Workout.find({}).sort({day:-1}).limit(1).then(workoutRes => {
+  db.Workout.find().sort({$natural:-1}).limit(1).then(workoutRes => {
     res.json(workoutRes);
     }).catch(err => {
     res.json(err);
