2314d6e27045d8fac9f90663afd812f33148cc87
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2020-11-10 21:56:06 -0800

    deploying

--- server.js
+++ server.js
@@ -11,9 +11,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
-  useNewUrlParser: true
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 
 app.listen(PORT, () => {
