89033ca519de7c82412f24f1518f547f9fd886bb
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2020-12-15 19:16:27 -0800

    trying new folder structure and adjusted API calls

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter an exercise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter an exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Please enter an exercise duration"
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+
+        }]
+});
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -22,74 +22,12 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts",
   }
 );
 
+const db = require("./models")
+
+require("./routes")
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
-
-
-// HTML Routes
-
-app.get("/", (req,res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/exercise", (req,res) => {
-  res.sendFile(path.join(__dirname, "./public/exercise.html"));
-});
-
-app.get("/stats", (req,res) => {
-  res.sendFile(path.join(__dirname, "./public/stats.html"))
-})
-
-
-
-// API ROUTES
-
-const db = require("./models")
-
-// https://mongoosejs.com/docs/models.html
-app.post("/api/workouts", (req, res) => {
-  let data = req.body;
-  db.Workout.create(data).then(update => {
-      res.json(update);
-  }).catch(err => {
-      res.json(err);
-  })
-});
-
-// https://docs.mongodb.com/manual/reference/operator/update/push/
-app.put("/api/workouts/:id", (req,res) => {
-  let dataId= req.params.id;
-  let data = req.body;
-  
-  db.Exercise.create(data).then(() =>
-      db.Workout.findOneAndUpdate(
-          {_id: dataId},
-          {$push: {exercises: _id}},
-          {new: true}
-      )).then( update => {
-          res.json(update);
-      }).catch(err => {
-          res.json(err)
-      })
-});
-
-
-app.get("/api/workouts/range", (req, res) => {
-  db.Workout.find().then(workoutRes => {
-    res.json(workoutRes);
-  }).catch(err => {
-    res.json(err);
-  })
-})
-
-// https://stackoverflow.com/questions/10920651/get-the-latest-record-from-mongodb-collection/53474483
-app.get("/api/workouts", (req,res) => {
-  db.Workout.find().limit(1).then(workoutRes => {
-    res.json(workoutRes);
-    }).catch(err => {
-    res.json(err);
-  })
-});
-
